113e55e96b6d7e028ca12104c91af867ee738e21
YALB <32033393+YALB@users.noreply.github.com>
2017-09-17 14:30:40 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include_once('name-wird-nicht-verraten.php');
+error_reporting(E_ALL-E_NONE);
+$sicherheitsvariable = 314259256;
+include_once('preincluder.php');
+
+//
+// hier kommt noch mehr 
+//
+
+?>
